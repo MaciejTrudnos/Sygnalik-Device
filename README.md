c546d602375a21cc5c146acea73a3748c74f4516
Maciej Trudnos <mtrudnos@inseqr.pl>
2025-10-11 23:05:26 +0200

    Display specific icons based on incoming message type

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Sygnalik-Device
-The device receives smartphone notifications via Bluetooth.
+# Sygnalik-Device  
+
+The Sygnalik-Device receives smartphone messages via Bluetooth from the [Sygnalik-App](https://github.com/MaciejTrudnos/Sygnalik-App) and displays them on the device.  
+
+## Supported notification types  
+- 📩 SMS
+- 📞 Incoming calls
+- 🚨 Speed camera alerts
 
 ## Dev Module Configuration in Arduino IDE  
 
@@ -14,3 +20,26 @@ The device receives smartphone notifications via Bluetooth.
 | **Flash Size**                   | 4MB (32Mb)                                 |
 | **Partition Scheme**             | Huge APP (3MB No OTA / 1MB SPIFFS)         |
 | **JTAG Adapter**                 | Integrated USB JTAG                        |
+
+## Image Converter to use in LVGL
+
+1. Go to [Image Converter](https://lvgl.io/tools/imageconverter)
+2. Select image (BMP, JPG, PNG, or SVG)
+3. Set color format: `CF_TRUE_COLOR`
+4. Set output format: `C array`
+5. Convert and save file with `*.cpp` extension
+6. In your file update the const variable using the following format:
+
+```
+const lv_img_dsc_t variablename = {
+    .header = {
+        .cf = LV_IMG_CF_TRUE_COLOR,
+        .always_zero = 0,
+        .reserved = 0,
+        .w = 240,
+        .h = 240,
+    },
+    .data_size = 57600 * LV_COLOR_SIZE / 8,
+    .data = variablename_map,
+};
+```
--- Sygnalik/Sygnalik.ino
+++ Sygnalik/Sygnalik.ino
@@ -4,6 +4,11 @@
 #include <BLEServer.h>
 #include <Arduino.h>
 #include "CST816D.h"
+#include "sms.cpp"
+#include "phone.cpp"
+#include "ok.cpp"
+#include "connectdevice.cpp"
+#include "speed.cpp"
 
 #define I2C_SDA 4
 #define I2C_SCL 5
@@ -12,42 +17,52 @@
 
 CST816D touch(I2C_SDA, I2C_SCL, TP_RST, TP_INT);
 
-bool deviceConnected = false;
-String message = "Brak powiadomien";
-
-uint32_t bg_color = 0x000000;
+String message = "nonotifications";
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+bool update_img = true;
+lv_obj_t *img_obj;
 
-lv_obj_t *label;
+enum message_type {
+  donothing,
+  noconnection,
+  nonotifications,
+  textmessage,
+  call,
+  speedcamera
+};
 
-class MyCallbacks : public BLECharacteristicCallbacks 
-{
-  void onWrite(BLECharacteristic *pCharacteristic) 
-  {
+message_type message_state = noconnection;
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
     String rxValue = pCharacteristic->getValue().c_str();
 
-    if (rxValue.length() > 0) 
-    {
+    if (rxValue.length() > 0) {
       Serial.println(rxValue);
-      bg_color = 0x008000;
-      message = rxValue;
+      update_img = true;
+      if (rxValue == "sms") {
+        message_state = textmessage;
+      } else if (rxValue == "call") {
+        message_state = call;
+      } else if (rxValue == "speedcamera") {
+        message_state = speedcamera;
+      }
     }
   }
 };
 
-class MyServerCallbacks : public BLEServerCallbacks 
-{
-  void onConnect(BLEServer *pServer) 
-  {
-    deviceConnected = true;
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    update_img = true;
+    message_state = nonotifications;
     Serial.println("Urządzenie połączone!");
   };
 
-  void onDisconnect(BLEServer *pServer) 
-  {
-    deviceConnected = false;
+  void onDisconnect(BLEServer *pServer) {
+    update_img = true;
+    message_state = noconnection;
     Serial.println("Urządzenie rozłączone!");
 
     BLEDevice::startAdvertising();
@@ -59,9 +74,6 @@ void setup() {
   Serial.println("Start...");
   touch.begin();
   display_init();
-  label = lv_label_create(lv_scr_act());
-  lv_obj_set_style_text_color(label, lv_color_hex(0xFFFFFF), 0);
-  lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(bg_color), 0);
 
   pinMode(3, OUTPUT);
   digitalWrite(3, HIGH);
@@ -91,6 +103,11 @@ void setup() {
 
   BLEDevice::startAdvertising();
 
+  img_obj = lv_img_create(lv_scr_act());
+  lv_obj_align(img_obj, LV_ALIGN_CENTER, 0, 0);
+
+  lv_img_set_src(img_obj, &connectdevice);
+
   Serial.println("Setup done");
 }
 
@@ -98,29 +115,42 @@ void loop() {
   lv_timer_handler();
   delay(5);
 
-  if (deviceConnected) {
-    setLabel(message.c_str());
-    checkTouch();
+  if (message_state == noconnection && update_img) {
+    Serial.println("show connectdevice");
+    lv_img_set_src(img_obj, &connectdevice);
+    update_img = false;
   } else {
-    setLabel("Polacz z urzadzeniem");
-  }
-}
+    if (message_state == textmessage && update_img) {
+      Serial.println("show sms");
+      lv_img_set_src(img_obj, &sms);
+    } else if (message_state == call && update_img) {
+      Serial.println("show call");
+      lv_img_set_src(img_obj, &phone);
+    } else if (message_state == nonotifications && update_img) {
+      Serial.println("no notifications");
+      lv_img_set_src(img_obj, &ok);
+    } else if (message_state == speedcamera && update_img) {
+      Serial.println("speedcamera");
+      lv_img_set_src(img_obj, &speed);
+    }
 
-void setLabel(const char *txt) {
-  lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(bg_color), 0);
-  lv_label_set_text(label, txt);
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    update_img = false;
+    checkTouch();
+  }
 }
 
 void checkTouch() {
   uint16_t touchX, touchY;
   uint8_t gesture;
-  
+
   bool touched = touch.getTouch(&touchX, &touchY, &gesture);
-  
+
   if (touched) {
     Serial.printf("Ekran dotknięty! X:%d, Y:%d, gesture: %x\n", touchX, touchY, gesture);
-    bg_color = 0x000000;
-    message = "Brak powiadomien";
+
+    if (message_state != noconnection) {
+      message_state = nonotifications;
+      update_img = true;
+    }
   }
 }
